1b7f200663a465db4d923b8f0034ac3078ac1ed5
bike-rrr <tesla201299@gmail.com>
2023-03-18 22:42:15 -0500

    got bugs fixed => sort function was fucked...diff behavior on firefox vs chrome...

--- greedy-first-search.js
+++ greedy-first-search.js
@@ -16,7 +16,7 @@ const triedSquares = [];
 const optionList = [];
 
 const start = [6, 0];
-const end = [0, 11];
+const end = [0, 9];
 let count = 0;
 
 // RUN MAZE
@@ -81,8 +81,9 @@ function findOptions([y, x]) {
     
     // GREDDY FIRST SEARCH
     if (list.length > 1) {
+        console.log("GOTTA CHECK" , [y, x]);
         // console.log("BEFORE LLLLLLIIIIST: ", list);
-        greedyFirst(list);
+       list = greedyFirst(list);
         // return list.forEach(option => optionList.push(option))
     }
     // console.log("LLSSSSSSSSSSTEND ", list);
@@ -101,14 +102,27 @@ function spotTried([y,x]) {
 
 function greedyFirst(opt) {
     console.log("GREDDY BEFORE: ", opt)
-    opt.sort((a, b) => {
-        return distance(a) < distance(b);
-    })
-    opt.reverse();
+    const ratings = [];
     console.log("GREEEEEDY AFTER ", opt);
+    let tmp;
+    if (distance(opt[0]) > distance(opt[1])) {
+        ratings.push(opt[0]);
+        ratings.push(opt[1]);
+    } else {
+        ratings.push(opt[1]); 
+       ratings.push(opt[0]);
+    }
+    console.log(ratings, ' ratings')
+    return ratings;
 }
 
 function distance([y, x]) {
-    console.log((Math.abs(end[0] - y)) + (Math.abs(end[1] - x)) , " %%%%%%%% ABS")
+    console.log((Math.abs(end[0] - y)) + (Math.abs(end[1] - x)) , " %%%%%%%% ABS", [y,x])
     return Math.abs(end[0] - y) + Math.abs(end[1] - x)
 }
+
+
+// why does 4-4 not change
+// but 0-11 changes?
+
+// 0-11 and 4-4 changes now with a -b 
