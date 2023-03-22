8d4dc8cabca7cc13632e7484b968a3572bdc67fa
bike-rrr <tesla201299@gmail.com>
2023-03-21 21:22:08 -0500

    got slider working good for range

--- app/setup.js
+++ app/setup.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const start = [6, 0];
 const end = [0, 11];
 
+const timeouts = [];
 function setup(){
     const open = 'open';
     const closed = 'closed';
@@ -24,7 +25,17 @@ function setup(){
     // populate MAZE
     addColor(cs50, start, end);
     const mazeCoords = getCoordsList(start, end);
-    animateMoves(mazeCoords);
+    // animateMoves(mazeCoords);
+
+    // button algo clicks
+    document.querySelector("#breadth").addEventListener("click", () => {
+        for(let i = 0; i < timeouts.length; i++) {
+            clearTimeout(timeouts[i]);
+        }
+        document.querySelectorAll("td").forEach(cell => cell.style.backgroundColor = 'red');
+        animateMoves(mazeCoords,0);
+        console.log("click");
+    })
 }
 
 function addColor(cs50, start, end) {
@@ -54,18 +65,35 @@ function getCoordsList() {
 
     return squareList;
 }
+let speed = 320 - document.querySelector("input").value;
+const range = document.querySelector("input");
+range.addEventListener("input", (e) => speed = 320 - e.target.value)
 
-function animateMoves(mazeCoords) {
-   order.forEach((cell, index) => {
-    setTimeout(() => {
-            if ((atStart(order[index][0], order[index][1]) || (atEnd(order[index][0], order[index][1])))) {
-                return;
-            }
-
-            mazeCoords[cell[0]][cell[1]].style.backgroundColor = 'yellow';
+function animateMoves(mazeCoords, index) {
+    // order.forEach((cell, index) => {
+    // timeouts.push(setTimeout(() => {
+    //         if ((atStart(order[index][0], order[index][1]) || (atEnd(order[index][0], order[index][1])))) {
+    //             return;
+    //         }
+    //         mazeCoords[cell[0]][cell[1]].style.backgroundColor = 'yellow';
+    //         console.log(index)
         
-   }, 70 * index + 1);
-    })
+    // }, speed * index + 1));
+    //   });
+    //   console.log(timeouts[10], "fff");
+
+    timeouts.push(setTimeout(() => {
+        if (index >= order.length) {
+            return;
+        }
+        // console.log("index", index, order[index]);
+        const y = order[index][0];
+        const x = order[index][1];
+        console.log(y, x)
+
+        mazeCoords[y][x].style.backgroundColor = 'yellow';
+        animateMoves(mazeCoords, ++index);
+    }, speed));
 }
 
 function atStart(i, j) {
