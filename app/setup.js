9c7cf2464677d0ab2e7f0e94ed8ef3d9d7ade7f2
bike-rrr <tesla201299@gmail.com>
2023-03-22 20:27:56 -0500

    just cleaned code up..

--- app/setup.js
+++ app/setup.js
@@ -7,15 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
     setup();
 })
 
+// GLOBAL VARS
 const SELECT = document.querySelector("select");
 SELECT.addEventListener("change", () => {
     setup();
 })
+const TIMEOUTS = [];
 
-const timeouts = [];
 function setup(){
-    // const open = 'open';
-    // const closed = 'closed';
     const CURRENT_MAP = SELECT.value;
     const start = mazes[CURRENT_MAP].start;
     const end = mazes[CURRENT_MAP].end; 
@@ -31,87 +30,88 @@ function setup(){
             clearTimeouts();
             addColor(map, start, end);
             console.log(start, end, " foooop")
-            animateMoves(mazeCoords,0, breadthSearch(start, end, map), start, end);
+            animateMoves(mazeCoords,0, breadthSearch(start, end, map));
         });
 
     document.querySelector("#greedy").addEventListener("click", (e) => {
-        for(let i = 0; i < timeouts.length; i++) {
-                clearTimeout(timeouts[i]);
+        for(let i = 0; i < TIMEOUTS.length; i++) {
+                clearTimeout(TIMEOUTS[i]);
             }
             addColor(map, start, end);
-            animateMoves(mazeCoords,0, orderGreedy(start, end, map), start, end); 
+            animateMoves(mazeCoords,0, orderGreedy(start, end, map)); 
     })
 
        document.querySelector("#aStar").addEventListener("click", (e) => {
-        for(let i = 0; i < timeouts.length; i++) {
-                clearTimeout(timeouts[i]);
+        for(let i = 0; i < TIMEOUTS.length; i++) {
+                clearTimeout(TIMEOUTS[i]);
             }
             addColor(map, start, end);
-            animateMoves(mazeCoords,0, aStarSearch(start, end, map), start, end); 
+            animateMoves(mazeCoords,0, aStarSearch(start, end, map)); 
     })
-}
 
-function addColor(map, start, end) {
-    const rows = document.querySelectorAll('tr');
-    rows.forEach((row, i) => {
-        row.querySelectorAll("td").forEach((cell, j) => {
-            if (atStart(i, j, start)) {
-                cell.style.backgroundColor = 'red';
-            } else if (atEnd(i, j, end)) {
-                cell.style.backgroundColor = 'green';
+    // HELPER FUNCTIONS
+    function addColor(map) {
+        const rows = document.querySelectorAll('tr');
+        rows.forEach((row, i) => {
+            row.querySelectorAll("td").forEach((cell, j) => {
+                if (atStart(i, j)) {
+                    cell.style.backgroundColor = 'red';
+                } else if (atEnd(i, j)) {
+                    cell.style.backgroundColor = 'green';
+                } else {
+                    cell.style.backgroundColor = map[i][j] === 'open' ? 'black' : 'grey';
+                }
+            })
+        })
+    };
+    
+    function atStart(i, j) {
+        if (start[0] === i && start[1] === j) return true;
+    }
+    
+    function atEnd(i, j) {
+        if (end[0] === i && end[1] === j) return true;
+    }
+    
+    function getCoordsList() {
+        const squareList = [];
+        document.querySelectorAll('tr').forEach(row => {
+            const rowCells = [];
+            row.querySelectorAll('td').forEach(cell => {
+                rowCells.push(cell);
+            })
+            squareList.push(rowCells);
+        });
+    
+        return squareList;
+    }
+    
+    function clearTimeouts() {
+         for(let i = 0; i < TIMEOUTS.length; i++) {
+                    clearTimeout(TIMEOUTS[i]);
+                    console.log("clearing")
+                }
+    }
+    let speed = 220 - document.querySelector("input").value;
+    const range = document.querySelector("input");
+    range.addEventListener("input", (e) => speed = 220 - e.target.value)
+    
+    function animateMoves(mazeCoords, index, orderVersion, start, end) {
+        TIMEOUTS.push(setTimeout(() => {
+            if (index >= orderVersion.length) {
+                return;
+            }
+    
+            const y = orderVersion[index][0];
+            const x = orderVersion[index][1];
+    
+            if ((atStart(y, x, start) || atEnd(y, x, end))) {
             } else {
-                cell.style.backgroundColor = map[i][j] === 'open' ? 'black' : 'grey';
+                mazeCoords[y][x].style.backgroundColor = 'yellow';
             }
-        })
-    })
-};
+    
+            animateMoves(mazeCoords, ++index, orderVersion, start, end);
+        }, speed));
+    }
 
-function atStart(i, j, start) {
-    if (start[0] === i && start[1] === j) return true;
-}
-
-function atEnd(i, j, end) {
-    if (end[0] === i && end[1] === j) return true;
-}
-
-function getCoordsList() {
-    const squareList = [];
-    document.querySelectorAll('tr').forEach(row => {
-        const rowCells = [];
-        row.querySelectorAll('td').forEach(cell => {
-            rowCells.push(cell);
-        })
-        squareList.push(rowCells);
-    });
-
-    return squareList;
-}
-
-function clearTimeouts() {
-     for(let i = 0; i < timeouts.length; i++) {
-                clearTimeout(timeouts[i]);
-                console.log("clearing")
-            }
-}
-let speed = 220 - document.querySelector("input").value;
-const range = document.querySelector("input");
-range.addEventListener("input", (e) => speed = 220 - e.target.value)
-
-function animateMoves(mazeCoords, index, orderVersion, start, end) {
-    timeouts.push(setTimeout(() => {
-        if (index >= orderVersion.length) {
-            return;
-        }
-
-        const y = orderVersion[index][0];
-        const x = orderVersion[index][1];
-
-        if ((atStart(y, x, start) || atEnd(y, x, end))) {
-        } else {
-            mazeCoords[y][x].style.backgroundColor = 'yellow';
-        }
-
-        animateMoves(mazeCoords, ++index, orderVersion, start, end);
-    }, speed));
-}
-
+}    
