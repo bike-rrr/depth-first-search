d30f0eb28ce12ff613e35469d79bc1982cc6936f
bike-rrr <tesla201299@gmail.com>
2023-03-22 21:42:51 -0500

    got random map working...but it sucks lol

--- app/mazes.js
+++ app/mazes.js
@@ -17,7 +17,6 @@ mazes.cs50 = {
 
 }
 
-
 mazes.keith = {
     map: [
     [closed, closed, closed, open, open, open, open, closed, open, open, open, closed],
@@ -32,6 +31,37 @@ mazes.keith = {
     end: [3, 3]
 }
 
+function make() {
+    const list = [];
+    for (let i = 0; i < 7; i++){
+        let row = [];
+        for (let j = 0; j < 12; j++){
+            const rand = Math.random();
+            if (rand < .5) {
+                row.push(open);
+            } else {
+                row.push(closed)
+            }
+        }
+        list.push(row);
+    }
+    const spots = [];
+    const start = [];
+    const end = [];
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list[0].length; j++) {
+            if (list[i][j] === open) {
+                spots.push([i,j]);
+            }
+        }
+    } 
+    const randStart = spots[Math.floor(Math.random() * spots.length)];
+    spots.slice(randStart);
+    const randEnd = spots[Math.floor(Math.random() * spots.length)]; 
+    return {map:list, start:randStart, end:randEnd}
+}
+
+mazes.random = make();
 
 
 export {mazes};
